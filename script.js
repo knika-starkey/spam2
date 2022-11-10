006dfaf92081c7f004c58e351b07c8f4c5250107
knika-starkey <knika787@gmail.com>
2022-11-10 17:31:31 +0200

    first version

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let str1 = "Lorem ipsum dolor spAm amet consectetur.";
 let str2 = "Lorem ipsum dolor sit amet consectetur.";
 
-function replaceSpam(str) {
+function check(str) {
   let s = str.toLowerCase();
   let spam = "spam".toLowerCase();
-  return s;
+  return s.includes(spam);
 }
 
-alert(replaceSpam(str1));
-alert(replaceSpam(str2));
+alert(check(str1));
+alert(check(str2));
