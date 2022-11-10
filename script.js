e3ca9d92120eaa37ef9f4a1fd0e6c0f955bbaf63
knika-starkey <knika787@gmail.com>
2022-11-10 20:53:07 +0200

    for arr

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 let str1 = "Lorem ipsum dolor spAm amet consectetur.";
 let str2 = "Lorem ipsum dolor sit amet consectetur.";
 
-function check(str) {
-  let s = str.toLowerCase();
+function check(...str) {
+  let sOrig = [];
   let spam = "spam".toLowerCase();
-  return s.includes(spam);
+  // /gi
+  for (let i = 0; i < str.length; i++) {
+    sOrig[i] = str[i].split(" ");
+
+    for (let j = 0; j < sOrig[i].length; j++) {
+      if (sOrig[i][j].toLowerCase() == spam) {
+        sOrig[i][j] = "***";
+      }
+    }
+    sOrig[i] = sOrig[i].join(" ");
+  }
+
+  return sOrig;
 }
 
-alert(check(str1));
-alert(check(str2));
+alert(check(str1, str2));
+//alert(str1.split(" "));
